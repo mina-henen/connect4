5d86c580b6186b0d4e597dba0b59fad51aabff9c
mina-henen <mon.messi6@gmail.com>
2021-11-21 17:11:34 +0200

    game fix

--- src/connect4.java
+++ src/connect4.java
@@ -1,18 +1,18 @@
 public class connect4 {
 
-    public static char[][] grid = new char[7][6];
+    public static char[][] grid = new char[6][7];
 
     public static void printGame(){
         for (int i=5;i>-1;--i){
-            for (int j=0;j<6;++j)
-                System.out.print(grid[j][i]+" ");
+            for (int j=0;j<7;++j)
+                System.out.print(grid[i][j]+" ");
             System.out.println();
         }
     }
     public static void initializeGrid(){
         for (int i=0;i<6;++i) {
-            for (int j = 6; j > -1; --j)
-                grid[j][i] = '0';
+            for (int j =0; j<7;++j)
+                grid[i][j] = '0';
         }
     }
 
@@ -20,8 +20,8 @@ public class connect4 {
         if (n>6 || n<0)
             return false;
         for (int i=0;i<6;++i){
-            if (grid[n][i]=='0'){
-                grid[n][i]=p;
+            if (grid[i][n]=='0'){
+                grid[i][n]=p;
                 return true;
             }
         }
@@ -37,6 +37,8 @@ public class connect4 {
         putButton('2',3);
         putButton('1',3);
         putButton('2',4);
+        putButton('1',2);
+        putButton('1',5);
         printGame();
     }
 }
