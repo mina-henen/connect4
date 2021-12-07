471d694f52f28a17894cbcbd6f8b985e670ad50f
Mohamed Yasser <mohamed.y.ghanem@gmail.com>
2021-12-08 01:28:33 +0200

    Minimax without pruning

--- src/State.java
+++ src/State.java
@@ -1,15 +1,7 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
 public class State {
     public char[][] grid;
     private int utility;
-=======
-
-public class State {
-
-    public char[][] grid;
-    public int utility;
->>>>>>> 26e343a5caab83e6e7bafc4e7d1a1937d5d6c2fe
     public ArrayList<State> children;
     public int col;
 
@@ -23,12 +15,9 @@ public class State {
         this.utility = utility;
     }
 
-<<<<<<< HEAD
     public int getUtility(){
         return utility;
     }
-=======
->>>>>>> 26e343a5caab83e6e7bafc4e7d1a1937d5d6c2fe
     public void addChild(State s){
         children.add(s);
     }
@@ -36,8 +25,4 @@ public class State {
     public void setCol(int col) {
         this.col = col;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 26e343a5caab83e6e7bafc4e7d1a1937d5d6c2fe
