a531983ad837610da0e193afb92c7c7f96115b08
Mohamed Yasser <54010131+MohamedYasser2@users.noreply.github.com>
2021-12-08 20:08:06 +0200

    Add files via upload

---
+++ new project/Connect4/src/main/java/Algorithm/State.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+import java.util.ArrayList;
+public class State {
+    public char[][] grid;
+    private int utility;
+    public ArrayList<State> children;
+    public int col;
+
+    public State(char[][] grid) {
+        this.grid = grid;
+        col = -1;
+        children = new ArrayList<State>();
+    }
+
+    public void setUtility(int utility) {
+        this.utility = utility;
+    }
+
+    public int getUtility(){
+        return utility;
+    }
+    public void addChild(State s){
+        children.add(s);
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+}
